4850935b7a4577128af2637aa607e9b495d6a8e7
Daechler <50748803+Daechler@users.noreply.github.com>
2023-05-25 18:29:24 +0200

    Add files via upload

--- src/main/java/net/daechler/fullbright/Fullbright.java
+++ src/main/java/net/daechler/fullbright/Fullbright.java
@@ -1,5 +1,6 @@
 package net.daechler.fullbright;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -9,14 +10,26 @@ import org.bukkit.potion.PotionEffectType;
 
 public class Fullbright extends JavaPlugin {
     @Override
+    public void onEnable() {
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + getName() + " has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + getName() + " has been disabled!");
+    }
+
+    @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             if (command.getName().equalsIgnoreCase("fullbright")) {
                 if (player.hasPotionEffect(PotionEffectType.NIGHT_VISION)) {
                     player.removePotionEffect(PotionEffectType.NIGHT_VISION);
+                    player.sendMessage(ChatColor.RED + "Fullbright has been disabled!");
                 } else {
                     player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 0, false, false));
+                    player.sendMessage(ChatColor.GREEN + "Fullbright has been enabled!");
                 }
                 return true;
             }
